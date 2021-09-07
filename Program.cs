6e862dc196e9226bd87a4934a0f4642bef5706cf
guilhermeee87 <gmsgui1000@gmail.com>
2021-09-06 22:49:24 -0300

    primeira fase

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace CRUDio
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluirSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException ("Opção Invalida!!");
+                        
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por usar meu app :D");
+            Console.WriteLine("");
+        }
+
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Lista de Filmes");
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhum filme cadastrado :( ");
+                Console.WriteLine();
+                return;
+            }
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+                Console.WriteLine("#ID {0}: - {1}{2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*" : ""));
+                Console.WriteLine("");
+                Console.WriteLine("* = Excluido");
+                Console.WriteLine("");
+            }
+        }
+
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir novo filme");
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o titulo do Filme: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.WriteLine("Ano de lançamento: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quando e porque assistimos: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie novaSerie = new Serie (id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            repositorio.Insere(novaSerie); 
+
+
+        }
+
+        private static void ExcluirSerie()
+        {
+            Console.WriteLine("Digite o ID da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void VisualizarSerie()
+        {
+            Console.WriteLine("Digite o ID da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+            Console.WriteLine(serie);
+        }
+        private static void AtualizarSerie()
+        {
+            Console.WriteLine("Digite o ID da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o titulo do Filme: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.WriteLine("Ano de lançamento: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quando e porque assistimos: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie (id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescricao);
+            repositorio.Atualiza(indiceSerie, atualizaSerie); 
+
+
+        }
+        private static string ObterOpcaoUsuario()
+        {   
+            Console.WriteLine("Guilherme de Moraes apresenta:");
+            Console.WriteLine("Lista de Filmes que já vimos Juntos!");
+            Console.WriteLine("Escolha sua Opção:");
+
+            Console.WriteLine("1 - Listar Filmes");
+            Console.WriteLine("2 - Inserir novo Filme");
+            Console.WriteLine("3 - Atualizar Filme");
+            Console.WriteLine("4 - Excluir Filme");
+            Console.WriteLine("5 - Visualizar Filme");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+            
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+        
+    }
+}
